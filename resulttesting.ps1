197a62c122439df9d1f10d0e5d9490b5bc392962
Rahul Singh <rahulsingh.message@gmail.com>
2023-10-27 19:02:17 +0530

    Update resulttesting.ps1

--- resulttesting.ps1
+++ resulttesting.ps1
@@ -3,7 +3,7 @@ $jsonFilePath = "packer-file.json"
 
 Function  Configurationvalidation($path) {
   Write-Host "Wrting to the host" -ForegroundColor DarkBlue -BackgroundColor White
-  return true
+  return false
 }
 
 Configurationvalidation($jsonFilePath )
